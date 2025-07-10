b9b0ec461983aa3a41bd0cc93793c0ab41445cf2
Muhammad Faraz <farazmohammad.tech@gmail.com>
2025-07-10 08:08:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def transform_text(text):
 
   return " ".join(y)
 
-tfidf = pickle.load(open('.vectorizer.pkl', 'rb'))
-model = pickle.load(open('.model.pkl', 'rb'))
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Phishing Email Detection")
 input_sms = st.text_area("Enter the text")
